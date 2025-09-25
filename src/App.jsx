826dde47598e18f2a717267bb4f7b8139e589531
NavinTiwari767 <navin.tiwari.cse.2022@nist.edu>
2025-09-25 19:31:42 +0530

    App

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import Nav from "./components/Nav";
 import About from "./components/About";
-import Course from "./components/course";
+import Course from "./Components/Course";
 import Gallery from "./components/Gallery";
 const App = () => {
   return (
